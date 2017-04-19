fc0fdca9007f4348a206b9721eeaa126808d20fe
Thiago <thiago@Thiagos-MacBook-Air.local>
2017-04-19 14:46:48 -0400

    if statements added

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
 console.log ("User flipped " + cardOne);
 console.log ("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne, cardTwo); 
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You have found a match");
+	} else {
+		alert("Please Try again");
+	}
+} 
