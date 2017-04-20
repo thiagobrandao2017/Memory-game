7f33bd11b7e2914e8c52bdaecf8697923e0b803c
Thiago <thiago@Thiagos-MacBook-Air.local>
2017-04-20 17:17:26 -0400

    last portion of game

--- memory_game/main.js
+++ memory_game/main.js
@@ -20,11 +20,15 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ]
-
 var cardsInPlay = [];
 
+// worked with Dan for the checkForMatch function.
 
-var checkForMatch = function() {
+var checkForMatch = function(n) {      
+	var str = '[data-id="'+n+'"]';
+	var cardClicked = document.querySelectorAll(str)[0];		
+	cardClicked.setAttribute('src',cards[n].cardImage);	
+	
 	if(cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
       		alert("You found a match!");
@@ -34,15 +38,21 @@ var checkForMatch = function() {
 	}	
 }
 
-
-var flipCard = function(cardId) {	
+var flipCard = function() {	
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch(); 	
+	checkForMatch(cardId); 	
 }
-
-
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);		
+	}
+}
+createBoard();
